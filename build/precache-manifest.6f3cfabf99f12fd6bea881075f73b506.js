24b71be0ba40bfed788615750e66effdf488662a
Eniola Adio <niwani@wikiwoos-MacBook-Pro-2.local>
2024-06-06 21:55:28 +0100

    initial commit

---
+++ build/precache-manifest.6f3cfabf99f12fd6bea881075f73b506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d015af18283ddc8beee2",
+    "url": "/static/css/main.570503bd.chunk.css"
+  },
+  {
+    "revision": "d015af18283ddc8beee2",
+    "url": "/static/js/main.d015af18.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "2997a320124b40cbda24",
+    "url": "/static/js/2.2997a320.chunk.js"
+  },
+  {
+    "revision": "d9898a2f2791fa6ef766",
+    "url": "/static/js/runtime~main.d9898a2f.js"
+  },
+  {
+    "revision": "fd20b7f6437d082d5ae40a78e22d5208",
+    "url": "/index.html"
+  }
+];
