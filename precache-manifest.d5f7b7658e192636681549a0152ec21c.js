ca6f9ba7fd2f3408dcb5c54160ef2816e7d76a66
Eniola Adio <niwani@wikiwoos-MacBook-Pro-2.local>
2024-06-10 11:48:30 +0100

    Updates

---
+++ precache-manifest.d5f7b7658e192636681549a0152ec21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d015af18283ddc8beee2",
+    "url": "/TodoListApp-ReactJS/static/css/main.570503bd.chunk.css"
+  },
+  {
+    "revision": "d015af18283ddc8beee2",
+    "url": "/TodoListApp-ReactJS/static/js/main.d015af18.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/TodoListApp-ReactJS/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "2997a320124b40cbda24",
+    "url": "/TodoListApp-ReactJS/static/js/2.2997a320.chunk.js"
+  },
+  {
+    "revision": "67e1a71e77a5bfc9721f",
+    "url": "/TodoListApp-ReactJS/static/js/runtime~main.67e1a71e.js"
+  },
+  {
+    "revision": "6c169010ef863b1de0e989345ebab790",
+    "url": "/TodoListApp-ReactJS/index.html"
+  }
+];
